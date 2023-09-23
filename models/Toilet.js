2b365c273886c74ae68f2282bfb73986c8628e12
valerioprds <v.paradiso89@gmail.com>
2023-09-23 14:49:19 +0200

    database finally works

--- models/Toilet.js
+++ models/Toilet.js
@@ -44,16 +44,9 @@ const ToiletSchema = new mongoose.Schema({
 // gecode and create location
 ToiletSchema.pre("save", async function (next) {
 	//const loc = await geocoder.geocode(this.address);
-	this.location = {
-		type: "Point",
-		        //coordinates: this.location.coordinates
-
-		coordinates: [2.194360415675912, 41.402947745952986],
-		//formattedAddress: loc[0].formattedAddress,
-	};
+	
 
 	//Do not save address
-	this.address = undefined;
 	next();
 });
 
